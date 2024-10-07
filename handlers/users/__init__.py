50b6e946b85dc74408af008d7803188c8c2c5b20
davronovuz <davronovtatu@gmail.com>
2024-10-07 16:06:33 +0500

    tortinchi   ozgarish

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,4 +1,5 @@
 from . import admin_commands
+from . import statistika_user
 from . import help
 from . import start
 from . import echo
--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,9 +1,20 @@
 from aiogram import types
+from  loader import dp,bot
+from data.config import ADMINS
 
-from loader import dp
 
-
-# Echo bot
-@dp.message_handler(state=None)
+@dp.message_handler()
 async def bot_echo(message: types.Message):
-    await message.answer(message.text)
+    user_text = message.text
+    user_username=message.from_user.username
+    user_fullname=message.from_user.full_name
+    user_id=message.from_user.id
+
+    msg=f"Qadrli Admin foydalanuvchi  {message.from_user.full_name} botga xabar yubordi\n"
+    msg+=f"foydalanuvchi haqida malumot:\n"
+    msg+=f"Username: @{user_username}\n"
+    msg+=f"Fullname: {user_fullname}\n"
+    msg+=f"ID: {user_id}\n"
+    msg+=f"Xabar: {user_text}"
+    for admin in ADMINS:
+        await bot.send_message(chat_id=admin,text=msg)
